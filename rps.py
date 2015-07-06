e52510efaa96e7d0a39cb371ee6d3f4df394c1ca
wellingi <devonandianwelling@gmail.com>
2015-07-06 04:35:12 -0400

    V2
    Cleaned up Code

--- rps.py
+++ rps.py
@@ -1,90 +1,76 @@
-from random import randint
+import random
 import os
-import time
 
-#Win Totals
-pw=0
-cw=0
-ties=0
+
+#clear screen
+def erase():
+    os.system('clear')
+
+#gameplay
+def rps():
+
+    #What beats what
+    winner = {
+        "rock" : "scissors",
+        "paper" : "rock",
+        "scissors" : "paper",
+    }
+
+    #scores and loop
+    keep_playing=True
+    win=0
+    lose=0
+    ties=0
+
+    while keep_playing==True:
+        erase()
+        playerguess=raw_input(" Please enter Rock, Paper, or Scissors: ").lower() # Players Guess
+        computerguess=random.choice(["rock", "paper", "scissors"]) #Computer Guess
+
+        erase()
+        print " You guessed", playerguess
+        print ""
+        print " The computer guessed", computerguess
+        print ""
+
+        #Deciding who won
+        if playerguess == computerguess:
+            print " Its a Tie!"
+            ties += 1
+        elif winner[playerguess]==computerguess:
+            print " You win!"
+            win += 1
+        else:
+            print " You Lose"
+            lose += 1
+        print ""
+
+        #Play again statment
+        print" Would you like to play again? Yes or No?"
+        answer=raw_input(" >")
+        if answer == "no":
+            erase()
+            print " Thank you for playing! Here are your results!"
+            print " Total Wins:", win
+            print " Total Loses:",lose
+            print " Total Ties:",ties
+            keep_playing=False
+
 
 #Welcome Text
 print """
-Welcome to Rock, Paper, Scissors!
+    Welcome to Rock, Paper, Scissors!
+    ---------------------------------
+    Where Rock Beats Scissors,
+    Scissors Cuts Paper,
+    And Paper Cuts Rock!
+    ---------------------------------
+    Would you like to play? Yes or No?
 """
-#Rock, Paper, Scissors Script
-def rps():
-    print ""
-    #stating globals
-    global pw
-    global cw
-    global ties
-
-    #Guesses
-    playerguess=raw_input("Please enter Rock, Paper, or Scissors: ")
-    computerguess=randint(1,3)
-    #rock=1 paper=2 scissors=3
-    pg=playerguess
-    cg=computerguess
-    print"Computer is guessing"
-    time.sleep(2)
-
-    #printing computer guess
-    if computerguess==1:
-        print "Computer Guessed Rock!"
-    elif computerguess==2:
-        print "Computer Guessed Paper"
-    else:
-        print "Computer Guessed Scissors"
-
-    #determine winner
-    if (pg.lower()=='rock' and cg==1) or (pg.lower()=='paper' and cg==2) or (pg.lower()=='scissors' and cg==3):
-        print ""
-        ties=+1
-        print "Its a tie!"
-        print ""
-        time.sleep(1)
-        print"Would you like to play again?"
-        answer=raw_input("Yes or no?")
-        if answer.lower()=="yes":
-            os.system('clear')
-            rps()
-        else:
-            print "Thanks for Playing!"
-            print "Wins: ", pw
-            print "Loses: ", cw
-            print "Ties: ", ties
-    elif (pg.lower()=='rock' and cg==2) or (pg.lower()=='paper' and cg==1) or (pg.lower=='scissors' and cg==2):
-        print ""
-        pw=+1
-        print "You WIN!"
-        print ""
-        time.sleep(1)
-        print"Would you like to play again?"
-        answer=raw_input("Yes or no?")
-        if answer.lower()=="yes":
-            os.system('clear')
-            rps()
-        else:
-            print "Thanks for Playing!"
-            print "Wins: ", pw
-            print "Loses: ", cw
-            print "Ties: ", ties
-    else:
-        print ""
-        cw=+1
-        print "You LOSE!"
-        print ""
-        time.sleep(1)
-        print"Would you like to play again?"
-        answer=raw_input("Yes or no?")
-        if answer.lower()=="yes":
-            os.system('clear')
-            rps()
-        else:
-            print "Thanks for Playing!"
-            print "Wins: ", pw
-            print "Loses: ", cw
-            print "Ties: ", ties
+play=raw_input(" >").lower()
+if play=="yes":
+    rps()
+else:
+    print "Oh well!"
 
 
-rps()
